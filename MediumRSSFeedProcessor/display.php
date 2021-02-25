e7f68c2943d3fe0b90f3ea3f955ae7bfa5d85563
Outback Games <admin@outbackgames.com.au>
2021-02-25 17:55:07 +0930

    Added Medium RSS Feed Processor.
    Added simple front and back-end for the Medium RSS feed processor.

---
+++ MediumRSSFeedProcessor/display.php
@@ -0,0 +1,65 @@
+
+<!-- this script is intended as an example of how to use the data from medium collect. -->
+<?php
+//Script Created by Outback Games https://outbackgames.com.au
+    include_once 'medium-collect.php';
+?>
+<html>
+<head>
+<script src="https://use.fontawesome.com/6a0c141912.js"></script> <!--not neccessary but hey, FA is pretty much standard-->
+<!-- your usual head stuff -->
+</head>
+<body>
+<?php
+    $localCount = 0;
+    $maxCount = (sizeof($posts) - 1);
+    if($maxCount > 10){
+        $maxCount = 10;
+    }
+    //create your own post cleaner or
+    //use below.
+    foreach($posts as $post){
+        $postTitle = $post['title'];
+        $postLink = $post['link'];
+        $postDescfull = $post['description'];
+        $postThumb = $post['thumbnail'];
+
+        //Post Description Cleaning
+        $maxPostDescLength = 100;
+        $postDescFind = strstr($postDescfull,'<p>', false); //find the first paragraph tag in the RSS2Json Description as this is where we want to start our snippit.
+		$postDescTrim = substr($postDescFind,0,$maxPostDescLength).'...';
+		$postDescCleanFirstPass = str_replace("<p>", "", $postDescTrim); //clean the tag from the description, if need be, create a second pass to remove closing tag.
+		$postDesc = $postDescCleanFirstPass; // assign short name var to the final clean pass.
+
+        if($localCount != 0){
+            //Changes Padding on final post (post number 10)
+            //change the html formatting to your own needs. this is just example code.
+            //alternatively you can use the cleaned posts array to select your data.
+            if($localCount == $maxCount){
+                echo '<div style="padding-top:1%;">';
+                echo '      <h4><a href="'.$postLink.'" target="_blank">'.$postTitle.'</a></h4>';
+                echo '  <div style="display:inline-block; vertical-align:top;">';
+                echo '      <a href="'.$postLink.'" target="_blank"><span><img src="'.$postThumb.'" width="160px" alt="'.$postTitle.'" /></span></a>';
+                echo '  </div>';
+                echo '  <div style="display:inline-block;">';
+                echo '      <p>'.$postDesc.' <a href="'.$postLink.'" target="_blank"> Read More <i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>';
+                echo '  </div>';
+                echo '</div>';
+            }else if ($localCount < $maxCount ){
+				echo '<div style="padding-bottom:1%;">';
+                echo '      <h4><a href="'.$postLink.'" target="_blank">'.$postTitle.'</a></h4>';
+                echo '  <div style="display:inline-block; vertical-align:top;">';
+                echo '      <a href="'.$postLink.'" target="_blank"><span><img src="'.$postThumb.'" width="160px" alt="'.$postTitle.'" /></span></a>';
+                echo '  </div>';
+                echo '  <div style="display:inline-block;">';
+                echo '      <p>'.$postDesc.' <a href="'.$postLink.'" target="_blank"> Read More <i class="fa fa-chevron-right" aria-hidden="true"></i></a></p>';
+                echo '  </div>';
+				echo '</div>';
+            }
+        }
+        $localCount++;
+    }
+?>
+</body>
+
+</html>
---
+++ MediumRSSFeedProcessor/medium-collect.php
@@ -0,0 +1,22 @@
+<?php
+//Script Created by Outback Games https://outbackgames.com.au
+
+$rss_url = 'https://medium.com/feed/@YOUR_MEDIUM_USERNAME';
+$api_endpoint = 'https://api.rss2json.com/v1/api.json?rss_url=';
+$data = json_decode(file_get_contents($api_endpoint.urlencode($rss_url)), true);
+$posts[] = array();
+$count = 0;
+$maxPostCount = 10;
+if($data['status'] == 'ok'){
+    foreach($data['items'] as $item){
+        if($count > $maxPostCount){
+            break; //break loop.
+        }
+        $posts[] = $item;
+        $count++;
+    }
+}
+
+
+
+?>
